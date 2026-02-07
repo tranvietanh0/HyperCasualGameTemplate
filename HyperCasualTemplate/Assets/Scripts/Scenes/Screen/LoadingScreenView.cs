e73529c057416f85b7e6feaefddda5623a62f574
tranvietanh0 <tranvietanh0507@gmail.com>
2026-02-07 11:00:04 +0700

    Add new asset schemas and implement state machine interfaces for scene management

--- HyperCasualTemplate/Assets/Scripts/Scenes/Screen/LoadingScreenView.cs
+++ HyperCasualTemplate/Assets/Scripts/Scenes/Screen/LoadingScreenView.cs
@@ -1,12 +1,15 @@
 ﻿namespace HyperCasualGame.Scripts.Scenes.Screen
 {
     using Cysharp.Threading.Tasks;
+    using GameFoundationCore.Scripts.AssetLibrary;
     using GameFoundationCore.Scripts.Signals;
     using GameFoundationCore.Scripts.UIModule.ScreenFlow.BaseScreen.Presenter;
     using GameFoundationCore.Scripts.UIModule.ScreenFlow.BaseScreen.View;
     using UITemplate.Scripts.UserData;
     using UniT.Logging;
     using UnityEngine;
+    using UnityEngine.ResourceManagement.AsyncOperations;
+    using UnityEngine.ResourceManagement.ResourceProviders;
 
     public class LoadingScreenView : BaseView
     {
@@ -16,21 +19,33 @@
     [ScreenInfo(nameof(LoadingScreenView))]
     public class LoadingScreenPresenter : BaseScreenPresenter<LoadingScreenView>
     {
+        protected virtual string NextSceneName => "1.MainScene";
+        #region Inject
         private readonly UserDataManager userDataManager;
+        private readonly IGameAssets     gameAssets;
 
         public LoadingScreenPresenter(
             SignalBus      signalBus,
             ILoggerManager loggerManager,
-            UserDataManager userDataManager
+            UserDataManager userDataManager,
+            IGameAssets    gameAssets
         ) : base(signalBus, loggerManager)
         {
             this.userDataManager = userDataManager;
+            this.gameAssets      = gameAssets;
         }
+        #endregion
+
 
         public async override UniTask BindData()
         {
-            Debug.Log("LoadingScreenPresenter - BindData");
             await this.userDataManager.LoadUserData();
+            await this.LoadSceneAsync();
+        }
+
+        protected virtual AsyncOperationHandle<SceneInstance> LoadSceneAsync()
+        {
+            return this.gameAssets.LoadSceneAsync(this.NextSceneName);
         }
     }
 }
